f382513a7f07c22039eaabe5d05407bbd30d9a5e
Vincent Quillien <vincent.quillien@dougs.fr>
2022-06-24 11:55:06 +0200

    Rephrase HomePage introduction sentence

--- src/components/HomePageHeader.tsx
+++ src/components/HomePageHeader.tsx
@@ -1,27 +1,27 @@
-import React from "react";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
-import styles from "./HomepageHeader.module.css";
+import React from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import styles from './HomepageHeader.module.css';
 
 export default function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">
-          The composable state management library that keeps you <b>focused</b>
-        </p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/Introduction/getting-started"
-          >
-            Get Started
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+            <div className="container">
+                <h1 className="hero__title">{siteConfig.title}</h1>
+                <p className="hero__subtitle">
+                    The scalable state management that keeps you <b>focused</b>
+                </p>
+                <div className={styles.buttons}>
+                    <Link
+                        className="button button--secondary button--lg"
+                        to="/docs/Introduction/getting-started"
+                    >
+                        Get Started
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
 }
